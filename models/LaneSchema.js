191e6d2a9fa68504dbadb803f2fd8d9a6ad6212e
Halit Derya <halitderyaa@gmail.com>
2024-04-30 16:07:50 +0100

    all apis done

--- models/LaneSchema.js
+++ models/LaneSchema.js
@@ -1,25 +1,33 @@
-const mongoose = require('mongoose');
-const counter = require('./CounterSchema')
+const mongoose = require("mongoose");
+const counter = require("./CounterSchema");
 
 const LaneSchema = new mongoose.Schema({
-    active: { type: Boolean, default: true },
-    default: { type: Boolean, default: false },
-    id:{type:Number, default:0},
-    description: String,
-    name: { type: String, required: true },
-    order: Number
+  active: { type: Boolean, default: true },
+  default: { type: Boolean, default: false },
+  id: { type: Number, default: 0 },
+  description: String,
+  name: { type: String, required: true },
+  order: Number,
 });
 
-LaneSchema.pre('save', async function(next) {
-    const doc = this;
+LaneSchema.pre("save", async function (next) {
+  const doc = this;
+  if (doc.isNew) {
     try {
-        const cnt = await counter.findByIdAndUpdate({_id: 'entityId'}, {$inc: { seq: 1 }}, {new: true, upsert: true});
-        doc.id = cnt.seq;
-        next();
+      const cnt = await counter.findByIdAndUpdate(
+        { _id: "entityId" },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      );
+      doc.id = cnt.seq;
+      next();
     } catch (error) {
-        next(error);
+      next(error);
     }
+  } else {
+    next();
+  }
 });
 
-const Lane = mongoose.model('Lane', LaneSchema);
-module.exports=Lane;
+const Lane = mongoose.model("Lane", LaneSchema);
+module.exports = Lane;

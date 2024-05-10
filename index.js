2618cd903fb8e71175680fd5f9e24f2eca20fc15
Halit Derya <halitderyaa@gmail.com>
2024-05-10 10:33:32 +0100

    cors test#888

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ const io = socketIo(server, {
     credentials: true,
   },
 });
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 const cardRoutes = require("./routes/cards");
 const settingsRoutes = require("./routes/settings");

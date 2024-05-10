7c0f605970a640514c6c8344fbb4feedb295e6a9
Halit Derya <halitderyaa@gmail.com>
2024-05-10 09:30:53 +0100

    cors

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const port = process.env.NODE_ENV === "development" ? 4500 : 3000;
 
 const cors = require("cors");
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000", "http://kanban.halitd.com"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy violation"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 const connectionOptions = {
   dbName: `kanbanBoard`,
